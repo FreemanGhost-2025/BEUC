76cec396873d8b49a69e2002f7220979fff47747
FreemanGhost-2025 <ulrich@gojerakcorp.com>
2025-08-12 13:32:59 +0100

    Create cpt-reservation.php

---
+++ includes/cpt-reservation.php
@@ -0,0 +1,14 @@
+// Dans ton plugin Flash Reservation
+add_action('init', function() {
+    register_post_type('reservation', [
+        'labels' => [
+            'name' => 'Réservations',
+            'singular_name' => 'Réservation',
+            'menu_name' => 'Réservations',
+        ],
+        'public' => false,
+        'show_ui' => true,
+        'menu_icon' => 'dashicons-tickets-alt',
+        'supports' => ['title', 'custom-fields'],
+    ]);
+});
